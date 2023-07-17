4ec50ad1b2d90e45081cb90d0440be44243f7df6
lvpiqi <96166538+lvpiqi@users.noreply.github.com>
2023-07-17 18:33:41 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -37,7 +37,11 @@ export const getServerSideConfig = () => {
     );
   }
 
-  return {
+    const apiKeys = (process.env.OPENAI_APIKEY ?? ").split(',')
+    const apiKey = apiKeys.at(Math.floor(Math.random()* apiKeys.length)) ?? "
+    
+    return{
+    apiKey,
     apiKey: process.env.OPENAI_API_KEY,
     code: process.env.CODE,
     codes: ACCESS_CODES,

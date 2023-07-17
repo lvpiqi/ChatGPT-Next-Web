e7ac70a46b9485f151555553d6bd3b85e65c3f98
lvpiqi <96166538+lvpiqi@users.noreply.github.com>
2023-07-17 11:07:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
+  title: "智能写作助手",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
